8b0ad417bb5fe40982c892965504078b6e01fad6
eun6304 <a01050412736@gmail.com>
2022-09-06 00:06:54 +0900

    - landing page title 만들기

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'http://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+
+export const API_KEY = 'dcefb8002dfbbc14d7c41e437666871c'
